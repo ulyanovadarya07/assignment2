d34913bbc80bf8966556b52a6fac0bd0281a25f3
daryaulyanova <ulyanova.darya07@gmail.com>
2025-12-29 10:37:40 +0500

    Implemented Assignment 2

---
+++ src/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    private static int id_gen = 1;
+    private String name;
+    private String surname;
+    private double gpa;
+    /*public Student(){
+
+    }*/
+    public Student(String name, String surname, double gpa){
+        this.name = name;
+        this.surname = surname;
+        this.gpa = gpa;
+    }
+
+    public int getId(){
+        return id_gen;
+    }
+    public double GetGpa(){
+        return gpa;
+    }
+    public
+}
